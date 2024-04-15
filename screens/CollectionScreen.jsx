4d7c0de3896109b2db0aef8400b5d982d273a537
Aluno <jpe.bgi@gmail.com>
2024-04-15 15:35:27 -0300

    Finished CollectionScreen

--- screens/CollectionScreen.jsx
+++ screens/CollectionScreen.jsx
@@ -1,6 +1,6 @@
 import React, {useContext} from "react";
 import HeaderComponent from "../components/HeaderComponent";
-import {Button, FlatList, SafeAreaView, Text} from "react-native";
+import {Pressable, FlatList, SafeAreaView, Image, Text, View} from "react-native";
 
 import Collections from "../context/CollectionsContext";
 
@@ -12,14 +12,46 @@ export default function CollectionScreen({ navigation }){
         <SafeAreaView>
             <HeaderComponent navigation={navigation} />
             <FlatList
+                contentContainerStyle={{padding: 10, gap: 10}}
+                columnWrapperStyle={{gap: 10}}
                 numColumns={2}
                 data={collections}
                 renderItem={({item}) =>
-                    <Button
-                        title={item.name}
-                        onPress={() => {navigation.navigate("Gallery", {collection: item.name})}}
-                    />
-                } />
+                    <Pressable
+                        onPressOut={() => navigation.navigate("Gallery", {collection: item.name})}
+                        style={{flex: 1, aspectRatio: 1, position: 'relative'}}
+                    >
+                        <Image
+                            source={{uri: item.images[0]}}
+                            style={{width: '100%',
+                                aspectRatio: 1,
+                                borderRadius: 30,
+                                position: 'absolute',
+                                left: 0,
+                                top: 0
+                        }}
+                        />
+                        <View
+                            style={{width: '100%',
+                                aspectRatio: 1,
+                                borderRadius: 30,
+                                position: 'absolute',
+                                left: 0,
+                                top: 0,
+                                backgroundColor: 'rgba(0,0,0,0.65)'
+                        }}
+                        />
+                        <Text style={{
+                            position: 'absolute',
+                            left: '50%',
+                            bottom: 10,
+                            transform: [{translateX: '-50%'}],
+                            color: '#eee',
+                            fontSize: 25
+                        }}>{item.name}</Text>
+                    </Pressable>
+                }
+                />
         </SafeAreaView>
     )
 }
--- screens/GalleryScreen.jsx
+++ screens/GalleryScreen.jsx
@@ -8,7 +8,6 @@ import CardImagem from "../components/gallery/CardImagem";
 import AreaButtons from "../components/gallery/AreaButtons";
 
 
-
 export default function GalleryScreen({ route, navigation }){
     // Esse parâmetro serve para passar qual galeria que está aberta, é importante, não apague
     const {collection} = route.params
